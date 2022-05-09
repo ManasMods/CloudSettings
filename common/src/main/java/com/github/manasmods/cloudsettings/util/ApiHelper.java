becbc16ae413e5ea38434d62242b64c0e3572241
Christian Raoulis <craoulis@googlemail.com>
2022-05-09 12:19:55 +0200

    fix ApiHelper over all lol

--- common/src/main/java/com/github/manasmods/cloudsettings/util/ApiHelper.java
+++ common/src/main/java/com/github/manasmods/cloudsettings/util/ApiHelper.java
@@ -1,10 +1,12 @@
 package com.github.manasmods.cloudsettings.util;
 
 import com.github.manasmods.cloudsettings.handler.SettingsLoadingHandler;
+import com.github.manasmods.cloudsettings.lwjgl.AuthenticationWindow;
 import com.google.common.collect.Lists;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
 import net.minecraft.client.Minecraft;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
@@ -17,6 +19,8 @@ import org.lwjgl.util.tinyfd.TinyFileDialogs;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -80,7 +84,7 @@ public class ApiHelper {
     }
 
     private static HttpGet authorizedGet(String path) {
-        HttpGet request = new HttpGet("http://134.255.220.120:8800/" + path);
+        HttpGet request = new HttpGet("http://134.255.220.120:8800/cloudsettings/" + path);
         request.addHeader(HttpHeaders.USER_AGENT, "Googlebot");
         request.addHeader("user-token", SettingsLoadingHandler.apiToken.get());
         return request;
@@ -88,15 +92,27 @@ public class ApiHelper {
 
     public static boolean sendSetting(String key, String value) {
         try {
-            String urlPath = URLEncoder.encode(String.format("store/%s/%s/%s", getUserId(), key, value), "UTF-8");
-            CloseableHttpResponse response = HTTP_CLIENT.execute(authorizedGet(urlPath));
+            CloseableHttpResponse response = HTTP_CLIENT.execute(authorizedGet(String.format("store/%s/%s/%s", getUserId(), encode(key), encode(value))));
 
             HttpEntity entity = response.getEntity();
-            JsonObject resultObject = new JsonParser().parse(EntityUtils.toString(entity)).getAsJsonObject();
+
+            LogHelper.getLogger().info("Got {} as response", EntityUtils.toString(entity));
+
+            JsonReader reader = new JsonReader(new StringReader(EntityUtils.toString(entity)));
+            reader.setLenient(true);
+            JsonObject resultObject = new JsonParser().parse(reader).getAsJsonObject();
+
             String resultValue = getResultValue(resultObject);
+
+            if (resultValue.startsWith("Login failed")) {
+                SettingsLoadingHandler.getLoginKeyFile().get().delete();
+                AuthenticationWindow window = new AuthenticationWindow(Minecraft.getInstance().getUser().getUuid());
+                window.show();
+                return sendSetting(key, value);
+            }
             return resultValue != null && resultValue.equals("Value saved");
-        } catch (IOException e) {
-            LogHelper.getLogger().error("Failed to send setting {} to Cloud.", key);
+        } catch (Exception e) {
+            LogHelper.getLogger().trace("Exception while trying to send the update to the cloud.", e);
             return false;
         }
     }
@@ -105,9 +121,16 @@ public class ApiHelper {
         try (CloseableHttpResponse response = HTTP_CLIENT.execute(authorizedGet(String.format("store/%s", getUserId())))) {
             HttpEntity entity = response.getEntity();
             JsonObject resultObject = new JsonParser().parse(EntityUtils.toString(entity)).getAsJsonObject();
-            String resultValue = getResultValue(resultObject);
+            String resultValue = getResultValue(resultObject.get("result").getAsJsonObject());
             //Exit if no status has been sent
-            if (resultValue == null || !resultValue.equals("Success")) return Lists.newArrayList();
+            if (resultValue == null) return Lists.newArrayList();
+            if (resultValue.startsWith("Login failed")) {
+                SettingsLoadingHandler.getLoginKeyFile().get().delete();
+                AuthenticationWindow window = new AuthenticationWindow(Minecraft.getInstance().getUser().getUuid());
+                window.show();
+                return loadSettings();
+            }
+            if (!resultValue.equals("Success")) return Lists.newArrayList();
             //Exit if no values has been sent
             if (!resultObject.has("entries")) return Lists.newArrayList();
             JsonArray storedSettings = resultObject.get("entries").getAsJsonArray();
@@ -139,5 +162,9 @@ public class ApiHelper {
     private static String getResultValue(JsonObject jsonObject) {
         if (!jsonObject.has("result")) return null;
         return jsonObject.get("result").getAsString();
+    }
+
+    private static String encode(String value) throws UnsupportedEncodingException {
+        return URLEncoder.encode(value, "UTF-8");
     }
 }
