fa19b35f284c7385d4de71303ccdee73fd650583
Christian Raoulis <craoulis@googlemail.com>
2022-07-15 18:30:50 +0200

    use SSL encryption and domain instead of direct IP

--- common/src/main/java/com/github/manasmods/cloudsettings/util/ApiHelper.java
+++ common/src/main/java/com/github/manasmods/cloudsettings/util/ApiHelper.java
@@ -30,6 +30,7 @@ import java.util.stream.Stream;
 
 public class ApiHelper {
     private static final CloseableHttpClient HTTP_CLIENT = HttpClients.createDefault();
+    private static final String apiEndpointUrl = "https://cloudservice.blutmondgilde.de:2053";
 
     public static String checkAutoLogin() {
         if (!SettingsLoadingHandler.getLoginKeyFile().exists()) {
@@ -44,7 +45,7 @@ public class ApiHelper {
                 return null;
             }
             LogHelper.getLogger().info("Trying login with the Token...");
-            HttpGet request = new HttpGet("http://134.255.220.120:8800/cloudsettings/autologin");
+            HttpGet request = new HttpGet(apiEndpointUrl + "/cloudsettings/autologin");
             request.addHeader(HttpHeaders.USER_AGENT, "Googlebot");
             request.addHeader("user-token", storedToken);
 
@@ -68,7 +69,7 @@ public class ApiHelper {
     }
 
     public static String getUserApiToken(String uuid, String password) {
-        HttpGet request = new HttpGet("http://134.255.220.120:8800/cloudsettings/login/" + uuid);
+        HttpGet request = new HttpGet(apiEndpointUrl + "/cloudsettings/login/" + uuid);
         request.addHeader(HttpHeaders.USER_AGENT, "Googlebot");
         request.addHeader("user-password", password);
         try (CloseableHttpResponse response = HTTP_CLIENT.execute(request)) {
@@ -97,7 +98,7 @@ public class ApiHelper {
     }
 
     private static HttpGet authorizedGet(String path) {
-        HttpGet request = new HttpGet("http://134.255.220.120:8800/cloudsettings/" + path);
+        HttpGet request = new HttpGet(apiEndpointUrl + "/cloudsettings/" + path);
         request.addHeader(HttpHeaders.USER_AGENT, "Googlebot");
         request.addHeader("user-token", SettingsLoadingHandler.apiToken.get());
         return request;
