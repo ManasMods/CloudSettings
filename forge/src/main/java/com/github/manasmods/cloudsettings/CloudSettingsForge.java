cff6ce144420ee2690cc5dda127883ee06b7d8e9
Christian Raoulis <craoulis@googlemail.com>
2023-01-03 19:16:21 +0100

    remove unnecessary forge stuff

--- forge/src/main/java/com/github/manasmods/cloudsettings/CloudSettingsForge.java
+++ forge/src/main/java/com/github/manasmods/cloudsettings/CloudSettingsForge.java
@@ -1,21 +1,6 @@
 package com.github.manasmods.cloudsettings;
 
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
-import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("cloudsettings")
-public class CloudSettingsForge {
-    public CloudSettingsForge() {
-        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        modEventBus.addListener(this::clientInit);
-    }
-
-    private void clientInit(final FMLClientSetupEvent e) {}
-}
+public class CloudSettingsForge {}
