9c583c5965b29a5421ccb30c2516d635f91d692e
Christian Raoulis <craoulis@googlemail.com>
2023-01-04 14:07:36 +0100

    add sub modules

---
+++ fabric_1_18_2/src/main/java/com/github/manasmods/cloudsettings/CloudSettingsFabric.java
@@ -0,0 +1,9 @@
+package com.github.manasmods.cloudsettings;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CloudSettingsFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
